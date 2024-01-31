18d7f5f364d6b3d53d30e4f31ad09ec26f3d21be
coulterminer <coultercash@gmail.com>
2024-01-31 12:11:33 -0700

    [API]: Start Building Transcribe Integration

---
+++ API/__init__.py
@@ -0,0 +1,8 @@
+import boto3
+
+class AWSTranscribe:
+    def __init__(self):
+        self.client = boto3.client("transcribe", region_name="us-west-2")
+
+    def transcribe_message(self, sender_client):
+        None
