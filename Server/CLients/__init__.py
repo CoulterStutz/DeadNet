8238d96e1a88cb467560714c15663cc3dee2c193
coulterminer <coultercash@gmail.com>
2024-01-31 16:04:15 -0700

    Delete Server/CLients/__init__.py

--- Server/CLients/__init__.py
+++
@@ -1,8 +0,0 @@
-# Used for taking and handling the client
-
-class Client():
-  def __init__(self, client, vimIdentifier, hasRadarDetector:bool=False, role:str="Driver"):
-    self.client = client
-    self.vim = vimIdentifier
-    self.hasRadarDetector = hasRadarDetector
-    self.role = role
