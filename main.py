ef01b3e5b26f3a1c9f14b8110c5e5e2361f62817
coulterminer <coultercash@gmail.com>
2024-01-31 12:39:15 -0700

    [MAIN]: Switch to Socket

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-import API
-import Clients
-import Parser
-import Leaderboards
-from flask import *
+import threading, socket
+import API, Clients, Parser, Leaderboards
+from termcolor import colored
+
+def handle_client(client):
+    None
+
+def start_server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('0.0.0.0', 666))
+    server.listen(5)
+    print(f"[{colored('D3XDNET', 'red')}]: Listening on 0.0.0.0:666")
+
+    while True:
+        client, addr = server.accept()
+        print(f"[*] Accepted connection from {addr[0]}:{addr[1]}")
+
+        client_handler = threading.Thread(target=handle_client, args=(client,))
+        client_handler.start()
