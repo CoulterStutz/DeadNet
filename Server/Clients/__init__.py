7c8cf4284518e55ba5f316e403109cd4ba2c056b
coulterminer <coultercash@gmail.com>
2024-01-31 16:32:18 -0700

    [CLIENTS]: Formatting For PEP

--- Server/Clients/__init__.py
+++ Server/Clients/__init__.py
@@ -1,18 +1,20 @@
 # Used for taking and handling the client
 import json
 
+
 class Client():
-  def __init__(self, client, vimIdentifier, hasRadarDetector:bool=False, role:str="Driver"):
-    self.client = client
-    self.vim = vimIdentifier
-    self.hasRadarDetector = hasRadarDetector
-    self.role = role
+    def __init__(self, client, vimIdentifier, hasRadarDetector: bool = False, role: str = "Driver"):
+        self.client = client
+        self.vim = vimIdentifier
+        self.hasRadarDetector = hasRadarDetector
+        self.role = role
+
 
 def ClientAuthenticator(decodedData):
-  with open("clients.json", 'r') as j:
-    c = j.read()
-    clients = eval(c)
+    with open("clients.json", 'r') as j:
+        c = j.read()
+        clients = eval(c)
 
 
 if __name__ == "__main__":
-  ClientAuthenticator("1")
+    ClientAuthenticator("1")
