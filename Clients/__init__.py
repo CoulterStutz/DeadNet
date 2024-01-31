00d67fc790c1ed433570f8855b1e90c3829745fa
coulterminer <coultercash@gmail.com>
2024-01-31 12:04:32 -0700

    [CLIENTS]: Add Indentifiers

--- Clients/__init__.py
+++ Clients/__init__.py
@@ -1,5 +1,8 @@
 # Used for taking and handling the client
 
 class Client():
-  def __init__(self):
-    None
+  def __init__(self, client, vimIdentifier, hasRadarDetector:bool=False, role:str="Driver"):
+    self.client = client
+    self.vim = vimIdentifier
+    self.hasRadarDetector = hasRadarDetector
+    self.role = role
