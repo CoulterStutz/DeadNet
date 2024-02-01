5d9677318afd4b635ea73d0163e3b1ad50269952
Coulter <coultercash@gmail.com>
2024-02-01 09:20:04 -0700

    [CONNECTION]: Populate Connection Class

--- Client/Connection/__init__.py
+++ Client/Connection/__init__.py
@@ -1,4 +1,10 @@
 import socket
 
 class Connection:
-    None
+    def __init__(self, ip_address, port):
+        self.ip_address = ip_address
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET)
+
+    def instate_connection(self):
+        self.sock.connect((self.ip_address, self.port))
