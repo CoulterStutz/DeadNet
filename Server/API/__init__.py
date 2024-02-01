a609d00c0a4000021bc8c36a907a6154f67b5e45
Coulter <coultercash@gmail.com>
2024-02-01 09:55:23 -0700

    [TRANSCRIBE]: Take Filepath

--- Server/API/__init__.py
+++ Server/API/__init__.py
@@ -4,5 +4,5 @@ class AWSTranscribe:
     def __init__(self):
         self.client = boto3.client("transcribe", region_name="us-west-2")
 
-    def transcribe_message(self, sender_client):
+    def transcribe_message(self, sender_client, file):
         None
