991d4cb07671b33a6bf4decb7219b7bcaeab66b1
coulterminer <coultercash@gmail.com>
2024-02-02 12:47:21 -0700

    [SERVER]: Accept Client Connections

---
+++ Server/server.py
@@ -0,0 +1,23 @@
+import socket
+import os
+from termcolor import colored
+def handle_client(sock):
+    authCode = sock.recv(1024)
+
+def main(address, port):
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((address, port))
+    server.listen(5)
+
+    print(f"[{colored('D3XDCOMMS', 'red')}]: Listening on port {colored(port), 'cyan'}")
+
+    while True:
+        client, addr = server.accept()
+        print(f"[{colored('D3XDCOMMS', 'green')}]: Accepted New Connection From {colored(addr[0], 'cyan')}")
+
+        # Start a new thread to handle the client
+        client_handler = threading.Thread(target=handle_client, args=(client,))
+        client_handler.start()
+
+if __name__ == "__main__":
+    main()
